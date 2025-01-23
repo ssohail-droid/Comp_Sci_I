32f81119d458ba56ba25f77b6100c7c65a45004a
ssohail-droid <ahmeds12@go.lisd.net>
2025-01-22 22:16:14 -0600

    Add files via upload

---
+++ Fruit.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class Fruit here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Fruit
+{
+    public static void main (String[] args){
+        byte Apples = 35;
+        short Oranges = 22;
+        int Peaches = 18;
+        long Grapes = 10L;
+        byte Bananas = 8;
+        byte Pineapples = 5;
+        System.out.println("             Totals            ");
+        System.out.println("-------------------------------");
+        System.out.println("Apples = " + Apples);
+        System.out.println("Oranges = " + Oranges);
+        System.out.println("Peaches = " + Peaches);
+        System.out.println("Grapes = " + Grapes);
+        System.out.println("Bananas = " + Bananas);
+        System.out.println("Pineapples = " + Pineapples);
+        }
+}
